b873da626919c60ee7727b4c12c05f10518b33f5
Ryan Galimova <rgalimova@connected.io>
2022-03-13 17:11:43 -0400

    dotenv

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,10 @@
+import dotenv from 'dotenv'
 import fs from 'fs'
 
+dotenv.config({ path: './.env' })
+
 const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_KEY}&query=37.8267,-122.4233`
+console.log(url)
 
 const main = (error, response) => {
   console.log(response.body.current)
