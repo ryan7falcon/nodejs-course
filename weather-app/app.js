bc9069f5b0b62a9cb87ef1c3c1dbe02c8c60f9ce
Ryan Galimova <rgalimova@connected.io>
2022-05-07 16:48:51 -0400

    test

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,6 +4,7 @@ import getWeather from './getWeather.js'
 printDebug('process.argv: ', process.argv)
 
 const address = process.argv[2]
+// test
 
 if (!address) {
   console.log('Please provide address')
