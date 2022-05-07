f6450889bbfae469a320255c9e6018ad1f50bcf5
ryan7falcon <ryan7falcon@gmail.com>
2022-05-07 16:50:47 -0400

    test1

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,7 +4,6 @@ import getWeather from './getWeather.js'
 printDebug('process.argv: ', process.argv)
 
 const address = process.argv[2]
-// test
 
 if (!address) {
   console.log('Please provide address')
